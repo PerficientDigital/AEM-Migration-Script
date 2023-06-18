4978db257d61371c290c848dc314795c59077a99
Andrew Chepurny <chip@prj-3.com>
2023-06-18 11:06:08 -0400

    Updates to the content template

--- migrate.groovy
+++ migrate.groovy
@@ -1,25 +1,8 @@
 #!/usr/bin/env groovy
-/*
-*  This program is free software: you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation, either version 3 of the License, or
-*  (at your option) any later version.
-*
-*  This program is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  You should have received a copy of the GNU General Public License
-*  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-
 /**
 *  Migration script for migrating legacy content using an AEM Content Package
 *  See: https://github.com/PerficientDigital/AEM-Migration-Script
 */
-@Grab('org.apache.commons:commons-lang3:3.12.0')
-
 @Grab('com.xlson.groovycsv:groovycsv:1.3')
 import static com.xlson.groovycsv.CsvParser.parseCsv
 
@@ -31,12 +14,18 @@ import groovy.xml.MarkupBuilder
 import groovy.json.JsonSlurper
 import groovy.time.TimeCategory
 import groovy.time.TimeDuration
+import groovy.transform.Field
 import groovy.ant.AntBuilder
 import groovy.xml.XmlSlurper
 
 import java.nio.file.Files
 import java.nio.file.StandardCopyOption
-import com.opencsv.CSVReader
+
+// configure settings here
+@Field 
+final SEPARATOR = ","
+@Field 
+final ENCODING = "UTF-8"
 
 start = new Date()
 
@@ -69,7 +58,7 @@ Map loadReplacements() {
 
         cfgs.each{cfg ->
             println "Processing configuration: ${cfg}"
-            def source = parseCsv(sf.getText("UTF-8"), separator: ",")
+            def source = parseCsv(sf.getText(ENCODING), separator: SEPARATOR)
             for (line in source) {
                 if(cfg.mode == 'mapping'){
                     replacements[line[cfg.sourceKey]] = line[cfg.targetKey]
@@ -108,7 +97,7 @@ Map loadTemplates() {
     return templates
 }
 
-void processPages(String source, String jcrRoot) {
+void processPages(File source, File jcrRoot) {
     
     def templates = loadTemplates()
     def replacements = loadReplacements()
@@ -117,7 +106,7 @@ void processPages(String source, String jcrRoot) {
     def count = 0
     def migrated = 0
     println 'Processing pages...'
-    for (pageData in parseCsv(pageFile.getText("UTF-8"), separator: ",")) {
+    for (pageData in parseCsv(pageFile.getText(ENCODING), separator: SEPARATOR)) {
 
         println "Processing page ${count + 1}"
 
@@ -133,12 +122,12 @@ void processPages(String source, String jcrRoot) {
             def template = templates[pageData['Template']]
             assert template : "Template ${pageData['Template']} not found!"
 
-            def sourceFile = new File(source,pageData['Source Path'])
+            def sourceFile = new File(source, pageData['Source Path'])
             assert sourceFile.exists() : "Source file ${sourceFile} not found!"
 
             println "Using template ${pageData['Template']} for ${sourceFile}"
 
-            def inXml = new XmlSlurper().parseText(sourceFile.getText("UTF-8"))
+            def inXml = new XmlSlurper().parseText(sourceFile.getText(ENCODING))
 
             def writer = new StringWriter()
             def outXml = new MarkupBuilder(writer)
@@ -147,11 +136,12 @@ void processPages(String source, String jcrRoot) {
             template.renderPage(pageData, inXml, outXml, replacements)
 
             println 'Creating parent folder...'
-            def targetFile = new File(jcrRoot,"${pageData['New Path']}${File.separator}.content.xml")
-            targetFile.getParentFile().mkdirs()
+            def targetFile = new File(jcrRoot.toString(), "${pageData['New Path'].toString()}${File.separator}.content.xml".toString())
+            targetFile.parentFile.mkdirs()
 
             println "Writing results to $targetFile"
-            targetFile.write(writer.toString(),"UTF-8")
+            targetFile.write(writer.toString(),ENCODING)
+
             migrated++
         } else {
             println 'No action required...'
@@ -162,129 +152,151 @@ void processPages(String source, String jcrRoot) {
     println "${count} pages processed and ${migrated} migrated in ${TimeCategory.minus(new Date(), start)}"
 }
 
-void processFiles(String source, String jcrRoot){
-    
-    def contentXml = '''<?xml version="1.0" encoding="UTF-8"?>
-<jcr:root xmlns:exif="http://ns.adobe.com/exif/1.0/" xmlns:photoshop="http://ns.adobe.com/photoshop/1.0/" xmlns:tiff="http://ns.adobe.com/tiff/1.0/" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:stEvt="http://ns.adobe.com/xap/1.0/sType/ResourceEvent#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dam="http://www.day.com/dam/1.0" xmlns:cq="http://www.day.com/jcr/cq/1.0" xmlns:jcr="http://www.jcp.org/jcr/1.0" xmlns:mix="http://www.jcp.org/jcr/mix/1.0" xmlns:nt="http://www.jcp.org/jcr/nt/1.0"
-    jcr:primaryType="dam:Asset">
-    <jcr:content
-        jcr:primaryType="dam:AssetContent">
-        <metadata
-            jcr:primaryType="nt:unstructured"/>
-        <related jcr:primaryType="nt:unstructured"/>
-    </jcr:content>
-</jcr:root>
-'''
-    def tika = new Tika()
-    def files = new File("work${File.separator}config${File.separator}file-mappings.csv")
-    def count = 0
-    def migrated = 0
-    println 'Processing files...'
-    for (fileData in parseCsv(files.getText("UTF-8"), separator: ",")) {
-        def assetRoot = new File(jcrRoot,fileData['Target'].toString())
-        def sourceFile = new File(source,fileData['Source'].toString())
-        def mimeType = tika.detect(sourceFile)
-        
+def processFiles(sourceDir, targetDir) {
+    def sourcePath = sourceDir.toPath()
+    def targetPath = targetDir.toPath()
+
+    Files.walk(sourcePath).forEach { sourceFile ->
+        def targetFile = targetPath.resolve(sourcePath.relativize(sourceFile))
+
+        if (Files.isDirectory(sourceFile)) {
+            Files.createDirectories(targetFile)
+        } else {
+            Files.createDirectories(targetFile.getParent())
+            Files.copy(sourceFile, targetFile, StandardCopyOption.REPLACE_EXISTING)
+        }
+
+        println "Copying ${sourceFile} to ${targetFile}"
+    }
+}
+
+void processFile(File sourceFile, File assetRoot, Tika tika) {
+    if (sourceFile.isDirectory()) {
+        def targetDir = new File(assetRoot, sourceFile.name)
+        targetDir.mkdirs()
+
+        for (file in sourceFile.listFiles()) {
+            def targetFile = new File(targetDir, file.name)
+            processFile(file, targetFile, tika)
+        }
+    } else {
+        // Process the file
+        def mimeType = "application/octet-stream" // Default MIME type for directories
+
         println "Processing Source: ${sourceFile} Target: ${assetRoot}"
-        
+
+        if (sourceFile.isFile()) {
+            try {
+                mimeType = tika.detect(sourceFile)
+            } catch (FileNotFoundException e) {
+                // Handle file not found exception
+                println "File not found: ${sourceFile}"
+                return
+            }
+        }
+
         println 'Creating original.dir XML...'
         def writer = new StringWriter()
         def originalDirXml = new MarkupBuilder(writer)
         originalDirXml.'jcr:root'('xmlns:jcr':'http://www.jcp.org/jcr/1.0','xmlns:nt':'http://www.jcp.org/jcr/nt/1.0','jcr:primaryType':'nt:file'){
             'jcr:content'('jcr:mimeType': mimeType, 'jcr:primaryType': 'nt:resource')
         }
-        def originalDir = new File(assetRoot,"_jcr_content${File.separator}renditions${File.separator}original.dir${File.separator}.content.xml")
-        originalDir.getParentFile().mkdirs()
-        originalDir.newWriter().withWriter { w ->
-            w << writer.toString()
-        }
-        
+        def originalDirPath = assetRoot.getAbsolutePath() + File.separator + "_jcr_content" + File.separator + "renditions" + File.separator + "original.dir"
+        def originalDir = new File(originalDirPath)
+        originalDir.mkdirs()
+        def contentXmlFile = new File(originalDir, ".content.xml")
+        println "Writing results to $contentXmlFile"
+        contentXmlFile.write(writer.toString(), ENCODING)
+
         println 'Copying original file...'
-        Files.copy(sourceFile.toPath(), new File(assetRoot,"_jcr_content${File.separator}renditions${File.separator}original").toPath(),StandardCopyOption.REPLACE_EXISTING,StandardCopyOption.COPY_ATTRIBUTES)
-        
-        println 'Writing .content.xml...'
-        new File(assetRoot,'.content.xml').newWriter().withWriter { w ->
-            w << contentXml
+        def targetFile = new File(assetRoot, sourceFile.name)
+        Files.copy(sourceFile.toPath(), targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
+    }
+}
+
+def newConfigDir = new File(args[0])
+assert newConfigDir.exists()
+println 'Copying configuration to work dir...'
+def workConfigDir = new File("work${File.separator}config")
+if (!workConfigDir.exists()) {
+    workConfigDir.mkdirs()
+}
+newConfigDir.eachFile { file ->
+    def destFile = new File(workConfigDir.getAbsolutePath() + File.separator + file.name)
+    def inputFile = new File(file.absolutePath)
+    def outputFile = new File(destFile.absolutePath)
+
+    outputFile.parentFile.mkdirs()
+
+    inputFile.withInputStream { inputStream ->
+        outputFile.withOutputStream { outputStream ->
+            outputStream << inputStream
         }
     }
 }
 
-def configDir = new File(args[0])
-assert configDir.exists()
-println 'Copying configuration to work dir...'
-def workConfig = new File("work${File.separator}config")
-if(!workConfig.exists()){
-    workConfig.mkdirs()
-}
-configDir.eachFile (FileType.FILES) { file ->
-    Files.copy(file.toPath(), new File(workConfig.getAbsolutePath()+File.separator+file.getName()).toPath(),StandardCopyOption.REPLACE_EXISTING,StandardCopyOption.COPY_ATTRIBUTES)
-}
 
 def base = new File('work')
-def source = new File(base, 'source')
-println "Using source: ${source.path}"
-def target = new File(base, 'target')
+def source = new File(base,'source')
+println "Using source: ${source}"
+def target = new File(base,'target')
 def jcrRoot = new File(target, 'jcr_root')
-println "Using target: ${target.path}"
+println "Using target: ${target}"
 
 println 'Clearing target...'
-target.deleteDir()
-target.mkdir()
-jcrRoot.mkdir()
+target.deleteDir();
+target.mkdir();
 
-// Create the parent directories of the target file
-def targetFile = new File(jcrRoot, '.content.xml')
-targetFile.parentFile.mkdirs()
-
-processPages(source.path,jcrRoot.path)
-processFiles(source.path,jcrRoot.path)
-
+processPages(source,jcrRoot)
+processFiles(new File("work${File.separator}source"), new File("work${File.separator}target${File.separator}jcr_root${File.separator}content${File.separator}dam"))
 
 println 'Updating filter.xml...'
-def vlt = new File(target,"META-INF${File.separator}vault")
+def vlt = new File("work/target/META-INF/vault")
 vlt.mkdirs()
-if(batch?.trim()){
+if (batch?.trim()) {
     def writer = new StringWriter()
     def filterXml = new MarkupBuilder(writer)
-    
-    def pageFile = new File("work${File.separator}config${File.separator}page-mappings.csv")
-    
-    filterXml.'workspaceFilter'('version':'1.0'){
-        for (pageData in parseCsv(pageFile.getText("UTF-8"), separator: ",")) {
-            if(batch.equals(pageData['Batch']) && 'Remove' != pageData[0] && 'Missing' != pageData[0]){
-                'filter'('root':pageData['New Path']){
-                    'include'('pattern':pageData['New Path'])
-                    'include'('pattern':"${pageData['New Path']}/jcr:content.*")
+
+    def pageFile = new File("work/config/page-mappings.csv")
+
+    filterXml.workspaceFilter(version: '1.0') {
+        for (pageData in parseCsv(pageFile.getText(ENCODING), separator: SEPARATOR)) {
+            if ('Remove' != pageData[0] && 'Missing' != pageData[0]) {
+                filterXml.filter(root: pageData['New Path']) {
+                    include(pattern: pageData['New Path'])
+                    include(pattern: "${pageData['New Path']}/jcr:content.*")
                 }
             }
         }
-        for (fileData in parseCsv(new File("work${File.separator}config${File.separator}file-mappings.csv").getText("UTF-8"), separator: ",")) {
-            if(batch.equals(fileData['Batch']) && 'Remove' != fileData[0] && 'Missing' != fileData[0]){
-                'filter'('root':fileData['Target']){
-                    'include'('pattern':fileData['Target'])
-                    'include'('pattern':"${fileData['Target']}/jcr:content.*")
+        for (fileData in parseCsv(new File("work/config/file-mappings.csv").getText(ENCODING), separator: SEPARATOR)) {
+            if ('Remove' != fileData[0] && 'Missing' != fileData[0]) {
+                filterXml.filter(root: fileData['Target']) {
+                    include(pattern: fileData['Target'])
+                    include(pattern: "${fileData['Target']}/jcr:content.*")
                 }
             }
         }
     }
-    new File('filter.xml',vlt) << writer.toString()
+    new File(vlt, 'filter.xml').withWriter { fileWriter ->
+        fileWriter << writer.toString()
+    }
 } else {
-    def filter = new File(workConfig, 'filter.xml')
+    def filter = new File('work/config/filter.xml')
     assert filter.exists()
-    Files.copy(filter.toPath(), new File(vlt, 'filter.xml').toPath(), StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES)
+    Files.copy(filter.toPath(), new File(vlt, 'filter.xml').toPath(), StandardCopyOption.REPLACE_EXISTING)
 }
 
 def now = new Date().format("yyyy-MM-dd-HH-mm-ss")
 println 'Updating properties.xml...'
-def propertiesXml = new File(workConfig,'properties.xml')
+def propertiesXml = new File(workConfigDir, 'properties.xml')
 assert propertiesXml.exists()
-new File(vlt,'properties.xml') << propertiesXml.getText().replace('${version}',now).replace('${name}',"migrated-content-${configDir.getName()}")
+new File(vlt, 'properties.xml') << propertiesXml.getText().replace('${version}',now).replace('${name}',"migrated-content-${workConfigDir.getName()}")
 
 println 'Creating package...'
 def ant = new AntBuilder()
-ant.zip(destfile: "${base.getAbsolutePath()}${File.separator}migrated-content-${configDir.getName()}-${now}.zip", basedir: target)
+ant.zip(destfile: "${base.getAbsolutePath()}${File.separator}migrated-content-${workConfigDir.getName()}-${now}.zip", basedir: target)
 
-println "Package saved to: work${File.separator}migrated-content-${configDir.getName()}-${now}.zip"
+println "Package saved to: work${File.separator}migrated-content-${workConfigDir.getName()}-${now}.zip"
 
 println "Content migrated in ${TimeCategory.minus(new Date(), start)}"
 
